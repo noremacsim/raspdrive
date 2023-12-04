628d50b2fa0b1d1c1d1318b58fcb0be06eec7a96
noremacsim <mrcameronsim@gmail.com>
2023-12-04 22:11:34 +0000

    Update Permissions

--- bin/mount_usb.sh
+++ bin/mount_usb.sh
@@ -2,15 +2,25 @@
 
 source /root/bin/envsetup.sh
 
-#if [ -e "/mnt/connectedUSB" ]; then
-#  if mountpoint -q /mnt/connectedUSB; then
-#      umount -f /mnt/connectedUSB
-#  fi
-#  mount $DATA_DRIVE /mnt/connectedUSB
-#else
-#  mkdir /mnt/connectedUSB
-#  mount $DATA_DRIVE /mnt/connectedUSB
-#fi
+if [ -e "/mnt/connectedUSB" ]; then
+  if mountpoint -q /mnt/connectedUSB; then
+      umount -f /mnt/connectedUSB
+  fi
+  mount $DATA_DRIVE /mnt/connectedUSB
+else
+  mkdir /mnt/connectedUSB
+  mount $DATA_DRIVE /mnt/connectedUSB
+fi
+
+if [ -e "/mnt/usbdata" ]; then
+  if mountpoint -q /mnt/usbdata; then
+      umount -f /mnt/usbdata
+  fi
+  mount /mnt/connectedUSB/usbdata.bin /mnt/usbdata
+else
+  mkdir /mnt/usbdata
+  mount /mnt/connectedUSB/usbdata.bin /mnt/usbdata
+fi
 
 /root/bin/disable_gadget.sh || true
 /root/bin/enable_gadget.sh || true
