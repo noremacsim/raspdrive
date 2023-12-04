d5c939b15108f00d490a75ebc79344a0e6d1bddb
noremacsim <mrcameronsim@gmail.com>
2023-12-04 18:12:39 +0000

    Update Permissions

--- boot/createBinUSB.sh
+++ boot/createBinUSB.sh
@@ -2,7 +2,17 @@
 
 source /root/bin/envsetup.sh
 
-usb_size=$(parted -s $DATA_DRIVE unit MB print | awk '$1 ~ /[0-9]/ {print $3}')
-dd bs=1M if=/dev/zero of=/mnt/connectedUSB/usbdata.bin count="$usb_size" status=progress
-#fallocate -l "${usb_size}" /mnt/connectedUSB/usbdata.bin
+# Get the size of $DATA_DRIVE in bytes using blockdev
+usb_size_bytes=$(blockdev --getsize64 "$DATA_DRIVE")
+
+# Subtract 4 gigabytes from the total size
+usb_size_bytes=$((usb_size_bytes - 4 * 1024 * 1024 * 1024))
+
+# Convert bytes to gigabytes
+usb_size_gigabytes=$(awk "BEGIN {print ${usb_size_bytes}/1024/1024/1024}")
+
+# Allocate space for the file using fallocate
+fallocate -l "${usb_size_gigabytes}G" /mnt/connectedUSB/usbdata.bin
+
+# Format the file with FAT32
 mkdosfs /mnt/connectedUSB/usbdata.bin -F 32 -I
