3ed56fbb958e3bab0ee1b4abd69a4e24e22accc7
noremacsim <mrcameronsim@gmail.com>
2023-12-04 16:16:46 +0000

    Update Permissions

--- boot/createBinUSB.sh
+++ boot/createBinUSB.sh
@@ -4,4 +4,5 @@ source /root/bin/envsetup.sh
 
 usb_size=$(parted -s $DATA_DRIVE unit MB print | awk '$1 ~ /[0-9]/ {print $3}')
 dd bs=1M if=/dev/zero of=/mnt/connectedUSB/usbdata.bin count="$usb_size" status=progress
+#fallocate -l "${usb_size}" /mnt/connectedUSB/usbdata.bin
 mkdosfs /mnt/connectedUSB/usbdata.bin -F 32 -I
